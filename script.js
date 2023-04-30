978d634910478846f6733ca32408174f83834c10
Ketevan <qeto71@gmail.com>
2023-04-30 18:32:03 +0400

    update

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 var number = 1;
 
-while(number <= 20) {
+while(number <= 100) {
     if(number % 2 == 0) {
-        document.write(number + " ლუწია! <br>");
+        //document.write(number + " ლუწია! <br>");
+    }else if(number % 2 == 1) {
+        document.write(number + " კენტია! <br>");
     }
 
     number++;
